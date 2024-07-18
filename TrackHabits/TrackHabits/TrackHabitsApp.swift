a1af7b00d90d059e1715906a851b4a02a8685366
galih <samudragalih94@gmail.com>
2024-07-18 14:09:24 +0700

    wip initial project

---
+++ TrackHabits/TrackHabits/TrackHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackHabitsApp.swift
+//  TrackHabits
+//
+//  Created by Galih Samudra on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
